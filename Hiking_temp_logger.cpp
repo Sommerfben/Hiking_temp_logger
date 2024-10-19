488e87c36f458db01213cedf42b66bdbc30b4be1
sommerfben <sommerfb@student.ubc.ca>
2024-10-18 18:15:07 -0700

    Removed extra file, cleaned up code formatting

--- Hiking_temp_logger.cpp
+++ Hiking_temp_logger.cpp
@@ -8,127 +8,130 @@
 #include "pico/cyw43_arch.h"
 #include "hardware/uart.h"
 #include "lib\temperature\temp_shtc3.h"
-#include <stdlib.h>  
+#include <stdlib.h>
 #include "lib\LCD\LCD_Driver.h"
 #include "lib\LCD\GUI_Paint.h"
 #include "rp2040_config.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+int64_t alarm_callback(alarm_id_t id, void *user_data)
+{
+   // Put your timeout handler code in here
+   return(0);
 }
 
 // Data will be copied from src to dst
 const char src[] = "Hello, world! (from DMA)";
-char dst[count_of(src)];
+char       dst[count_of(src)];
 
 
 int main()
 {
-    stdio_init_all();
+   stdio_init_all();
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
+   // Initialise the Wi-Fi chip
+   if (cyw43_arch_init())
+   {
+      printf("Wi-Fi init failed\n");
+      return(-1);
+   }
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 40*1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    
-    // GPIO init
-    gpio_init(DEV_CS_PIN); 
-    gpio_set_dir(DEV_CS_PIN, GPIO_OUT);
+   // SPI initialisation. This example will use SPI at 1MHz.
+   spi_init(SPI_PORT, 40 * 1000 * 1000);
+   gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+   gpio_set_function(PIN_CS, GPIO_FUNC_SIO);
+   gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+   gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
 
-    Config_Init();
-    LCD_Init();
-    Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 0, BLACK);
-    Paint_Clear(WHITE);
+   // GPIO init
+   gpio_init(DEV_CS_PIN);
+   gpio_set_dir(DEV_CS_PIN, GPIO_OUT);
 
-    // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
+   Config_Init();
+   LCD_Init();
+   Paint_NewImage(LCD_WIDTH, LCD_HEIGHT, 0, BLACK);
+   Paint_Clear(WHITE);
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+   // Chip select is active-low, so we'll initialise it to a driven-high state
+   gpio_set_dir(PIN_CS, GPIO_OUT);
+   gpio_put(PIN_CS, 1);
+   // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    // Get a free channel, panic() if there are none
-    int chan = dma_claim_unused_channel(true);
-    
-    // 8 bit transfers. Both read and write address increment after each
-    // transfer (each pointing to a location in src or dst respectively).
-    // No DREQ is selected, so the DMA transfers as fast as it can.
-    
-    dma_channel_config c = dma_channel_get_default_config(chan);
-    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-    channel_config_set_read_increment(&c, true);
-    channel_config_set_write_increment(&c, true);
-    
-    dma_channel_configure(
-        chan,          // Channel to be configured
-        &c,            // The configuration we just created
-        dst,           // The initial write address
-        src,           // The initial read address
-        count_of(src), // Number of transfers; in this case each is 1 byte.
-        true           // Start immediately.
-    );
-    
-    // We could choose to go and do something else whilst the DMA is doing its
-    // thing. In this case the processor has nothing else to do, so we just
-    // wait for the DMA to finish.
-    dma_channel_wait_for_finish_blocking(chan);
-    
-    // The DMA has now copied our text from the transmit buffer (src) to the
-    // receive buffer (dst), so we can print it out from there.
-    puts(dst);
+   // I2C Initialisation. Using it at 400Khz.
+   i2c_init(I2C_PORT, 400 * 1000);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+   gpio_pull_up(I2C_SDA);
+   gpio_pull_up(I2C_SCL);
+   // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+   // Get a free channel, panic() if there are none
+   int chan = dma_claim_unused_channel(true);
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+   // 8 bit transfers. Both read and write address increment after each
+   // transfer (each pointing to a location in src or dst respectively).
+   // No DREQ is selected, so the DMA transfers as fast as it can.
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+   dma_channel_config c = dma_channel_get_default_config(chan);
+   channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+   channel_config_set_read_increment(&c, true);
+   channel_config_set_write_increment(&c, true);
 
-    shtc3 temp_sensor;
+   dma_channel_configure(
+      chan,            // Channel to be configured
+      &c,              // The configuration we just created
+      dst,             // The initial write address
+      src,             // The initial read address
+      count_of(src),   // Number of transfers; in this case each is 1 byte.
+      true             // Start immediately.
+      );
 
-    uint16_t test_data = 0;
+   // We could choose to go and do something else whilst the DMA is doing its
+   // thing. In this case the processor has nothing else to do, so we just
+   // wait for the DMA to finish.
+   dma_channel_wait_for_finish_blocking(chan);
 
-    printf("Hello, world!\n");
-    float temp_data[2];
-    temp_sensor.make_low_power_measurement_blocking(temp_data);
-    printf("Temperature: %f\n", temp_data[0]);
-    printf("Humidity: %f\n", temp_data[1]);
+   // The DMA has now copied our text from the transmit buffer (src) to the
+   // receive buffer (dst), so we can print it out from there.
+   puts(dst);
 
-    while (true) {
-    }
+   // Timer example code - This example fires off the callback after 2000ms
+   add_alarm_in_ms(2000, alarm_callback, NULL, false);
+   // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+
+   printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
+   printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
+   // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+
+   // Example to turn on the Pico W LED
+   cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+
+   // Set up our UART
+   uart_init(UART_ID, BAUD_RATE);
+   // Set the TX and RX pins by using the function select on the GPIO
+   // Set datasheet for more information on function select
+   gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+   gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+   // Use some the various UART functions to send out data
+   // In a default system, printf will also output via the default UART
+
+   // Send out a string, with CR/LF conversions
+   uart_puts(UART_ID, " Hello, UART!\n");
+
+   // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+   shtc3 temp_sensor;
+
+   uint16_t test_data = 0;
+
+   printf("Hello, world!\n");
+   float temp_data[2];
+   temp_sensor.make_low_power_measurement_blocking(temp_data);
+   printf("Temperature: %f\n", temp_data[0]);
+   printf("Humidity: %f\n", temp_data[1]);
+
+   while (true)
+   {
+   }
 }
--- lib/temperature/temp_shtc3.cpp
+++ lib/temperature/temp_shtc3.cpp
@@ -7,71 +7,72 @@
 
 uint16_t shtc3::read_data(float *data, bool temperature_first)
 {
-    uint8_t regData[6];
-    uint8_t status; 
-    status = i2c_read_blocking(I2C_PORT, SHTC3_DEFAULT_ADDR, regData, 6, false); 
-    
-    float convertedTemperatureReading; 
-    float convertedHumidityReading; 
+   uint8_t regData[6];
+   uint8_t status;
 
-    // Extract data from read buffer and build the raw measurement values
-    if (temperature_first)
-    {
-        convertedTemperatureReading = ((regData[0] << 8) | regData[1]);
-        convertedHumidityReading = ((regData[3] << 8) | regData[4]);
-    }
-    else
-    {
-        convertedTemperatureReading = ((regData[3] << 8) | regData[4]);
-        convertedHumidityReading = ((regData[0] << 8) | regData[1]);
-    }
+   status = i2c_read_blocking(I2C_PORT, SHTC3_DEFAULT_ADDR, regData, 6, false);
 
-    // Convert raw measurement values to decimal 
-    convertedTemperatureReading = convertedTemperatureReading * 175;
-    convertedTemperatureReading = convertedTemperatureReading / 65536;
-    convertedTemperatureReading = convertedTemperatureReading - 45;
+   float convertedTemperatureReading;
+   float convertedHumidityReading;
 
-    convertedHumidityReading = convertedHumidityReading / 65536;
-    convertedHumidityReading = convertedHumidityReading * 100;
-    
-    data[0] = convertedTemperatureReading;
-    data[1] = convertedHumidityReading;
-    return 1;
+   // Extract data from read buffer and build the raw measurement values
+   if (temperature_first)
+   {
+      convertedTemperatureReading = ((regData[0] << 8) | regData[1]);
+      convertedHumidityReading    = ((regData[3] << 8) | regData[4]);
+   }
+   else
+   {
+      convertedTemperatureReading = ((regData[3] << 8) | regData[4]);
+      convertedHumidityReading    = ((regData[0] << 8) | regData[1]);
+   }
+
+   // Convert raw measurement values to decimal
+   convertedTemperatureReading = convertedTemperatureReading * 175;
+   convertedTemperatureReading = convertedTemperatureReading / 65536;
+   convertedTemperatureReading = convertedTemperatureReading - 45;
+
+   convertedHumidityReading = convertedHumidityReading / 65536;
+   convertedHumidityReading = convertedHumidityReading * 100;
+
+   data[0] = convertedTemperatureReading;
+   data[1] = convertedHumidityReading;
+   return(1);
 }
 
 /**
  * Performs an I2C write to send a command to the device
  *
  * @param   command   The 16-bit command ID to send.
- * @return  true if successful 
+ * @return  true if successful
  */
 bool shtc3::write_command(uint16_t command)
 {
-    uint8_t commandData[2]; 
-    commandData[0] = command >> 8;
-    commandData[1] = command & 0xFF;
-    return i2c_write_blocking(I2C_PORT, SHTC3_DEFAULT_ADDR, commandData, 2, false);
+   uint8_t commandData[2];
+
+   commandData[0] = command >> 8;
+   commandData[1] = command & 0xFF;
+   return(i2c_write_blocking(I2C_PORT, SHTC3_DEFAULT_ADDR, commandData, 2, false));
 }
 
 bool shtc3::make_low_power_measurement_blocking(float *data)
 {
-    write_command(SHTC3_WAKEUP); 
-    sleep_us(SHTC3_WAKE_TIME_US);
-    write_command(SHTC3_LOWPOW_MEAS_TFIRST);
-    sleep_us(SHTC3_LOW_POWER_MEAS_MAX_TIME_US);
-    read_data(data, true);
-    write_command(SHTC3_SLEEP);
-    return 1;
+   write_command(SHTC3_WAKEUP);
+   sleep_us(SHTC3_WAKE_TIME_US);
+   write_command(SHTC3_LOWPOW_MEAS_TFIRST);
+   sleep_us(SHTC3_LOW_POWER_MEAS_MAX_TIME_US);
+   read_data(data, true);
+   write_command(SHTC3_SLEEP);
+   return(1);
 }
 
 bool shtc3::make_high_power_measurement_blocking(float *data)
 {
-    write_command(SHTC3_WAKEUP);
-    sleep_us(SHTC3_WAKE_TIME_US);
-    write_command(SHTC3_NORMAL_MEAS_TFIRST);
-    sleep_ms(SHTC3_NORMAL_POWER_MEAS_MAX_TIME_MS);
-    read_data(data, true);
-    write_command(SHTC3_SLEEP);
-    return 1;
+   write_command(SHTC3_WAKEUP);
+   sleep_us(SHTC3_WAKE_TIME_US);
+   write_command(SHTC3_NORMAL_MEAS_TFIRST);
+   sleep_ms(SHTC3_NORMAL_POWER_MEAS_MAX_TIME_MS);
+   read_data(data, true);
+   write_command(SHTC3_SLEEP);
+   return(1);
 }
-
